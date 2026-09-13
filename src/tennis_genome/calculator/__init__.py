a88995aa7d8d2a2075882b2b255ed6fd0c47a760
Aidan Miller <aidandcmiller@live.com>
2026-09-13 13:10:27 -0400

    Export manual market comparison adapter

--- src/tennis_genome/calculator/__init__.py
+++ src/tennis_genome/calculator/__init__.py
@@ -1,6 +1,7 @@
 """Provider-neutral, market-blind matchup calculator."""
 
 from .engine import MatchupCalculator, fair_decimal_odds, load_neighbor_bank
+from .market import compare_manual_decimal_odds
 from .types import FairDecimalOdds, MatchupCalculation, MatchupInput
 
 __all__ = [
@@ -8,6 +9,7 @@ __all__ = [
     "MatchupCalculation",
     "MatchupCalculator",
     "MatchupInput",
+    "compare_manual_decimal_odds",
     "fair_decimal_odds",
     "load_neighbor_bank",
 ]
