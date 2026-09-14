222cdc06b02189385bb536ec8255bc2e4f659fe1
Aidan Miller <aidandcmiller@live.com>
2026-09-13 23:59:11 -0400

    Export workbench lineage contracts

--- src/tennis_genome/research_workbench/__init__.py
+++ src/tennis_genome/research_workbench/__init__.py
@@ -8,11 +8,23 @@ mutating TGE-Independent-v1.
 from .contracts import EvaluationSpec, ForecastingProcedureSpec, WorkbenchRecord
 from .evaluation import EvaluationResult, evaluate_probabilities
 from .exposure import ExposureGraph, ExposureKind, ExposureRecord
+from .lineage import (
+    CanonicalEvaluationBinding,
+    CodeFingerprint,
+    DatasetFingerprint,
+    ProcedureSearchFamily,
+    build_canonical_evaluation_binding,
+    fingerprint_code_components,
+    fingerprint_match_population,
+)
 from .registry import ImmutableResearchRegistry
 from .synthetic import SyntheticWorld, interaction_world, miscalibration_world, null_world
 from .synthetic_benchmark import SyntheticBenchmarkReport, run_synthetic_benchmark
 
 __all__ = [
+    "CanonicalEvaluationBinding",
+    "CodeFingerprint",
+    "DatasetFingerprint",
     "EvaluationResult",
     "EvaluationSpec",
     "ExposureGraph",
@@ -20,10 +32,14 @@ __all__ = [
     "ExposureRecord",
     "ForecastingProcedureSpec",
     "ImmutableResearchRegistry",
+    "ProcedureSearchFamily",
     "SyntheticBenchmarkReport",
     "SyntheticWorld",
     "WorkbenchRecord",
+    "build_canonical_evaluation_binding",
     "evaluate_probabilities",
+    "fingerprint_code_components",
+    "fingerprint_match_population",
     "interaction_world",
     "miscalibration_world",
     "null_world",
